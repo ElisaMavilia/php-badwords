4498bbac46d29b72ff55048f220c08ca031a89f6
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-29 14:26:26 +0200

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h2>Inserisci il testo</h2>
+    <form action="request.php" method="POST" name="">
+        <p><label for="badwords"></label></p>
+    <textarea id="badwords" name="" rows="4" cols="50">
+   </textarea>
+   <br>
+   <input type="submit" value="Submit"></input>
+    </form>
+   
+</body>
+</html>
