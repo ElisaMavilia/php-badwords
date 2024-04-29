7b6da5cbe4fd1922e9a63133a672bab5952fcf1f
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-29 15:20:21 +0200

    added str_replace

--- request.php
+++ request.php
@@ -22,5 +22,13 @@ $userParagraph =$_POST["userParagraph"];
     <p> Numero parole: 
     <?php  echo str_word_count($userParagraph) ?>
     </p>
+
+    <p>
+    ❝<em><?php  echo $userParagraph ?>❞</em>
+    </p>
+    <h2>Versione censurata: </h2>
+    <p>
+    <em>❝<?php  echo str_replace("come", "***", $userParagraph) ?>❞</em>
+    </p>
 </body>
 </html>
